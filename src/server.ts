a3f12ee3beed43f3d7e41ce7bb8ec01117f33d31
Tafhim301 <tafhim301@gmail.com>
2025-06-19 22:51:46 +0600

    Project initial setup has been completed

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+dotenv.config();
+import { Server } from "http";
+import app from "./app";
+import mongoose from "mongoose";
+let server: Server;
+const PORT = 5000;
+async function main() {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.gsjmw27.mongodb.net/library-manager-app?retryWrites=true&w=majority&appName=Cluster0`
+    );
+    console.log("Connected to MongoDB");
+    server = app.listen(PORT, () => {
+      console.log(`App is listening to port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
